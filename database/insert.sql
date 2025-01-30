9dcc72c5aa6d3fe482c2fa1a88954d3d16321bbc
Rue Ire <reerine20@msn.com>
2025-01-30 13:18:05 +0200

    create insert.sql
    just INSERT statements

---
+++ database/insert.sql
@@ -0,0 +1,11 @@
+/*ALL  are VARCHART
+Create backend possibility to add books/writers/comics
+Find out how to auto-add writer from books/comics to writer
+*/
+
+INSERT INTO writer(author, country) VALUES
+();
+INSERT INTO books(title, ISBN, genre, rating) VALUES
+();
+INSERT INTO comics(title, ISBN, genre, rating) VALUES
+();
