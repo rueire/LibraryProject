6eb8fcc02eebc6a7d83fa758adb9bb5e8a9a7e5f
Rue Ire <reerine20@msn.com>
2025-01-30 14:36:39 +0200

    Update database.py
    now should use .env
    not tested

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,30 @@
 # pip install pymysql
-# Not yet installed! will help with connection to mariabd
+# pip install python-dotenv
+# Not yet installed!
 
 import sqlite3
+# for .env:
+import os 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+# Get database credentials from environment variables
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 try:
-    connection = sqlite3.connect('library_db')
+    #connection = sqlite3.connect('library_db')
+    connection = pymysql.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    ) 
     print('Connection to library successful')
+    
 except sqlite3.Error as e:
     print('Error connecting to library database')
 
