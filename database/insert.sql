a98c546ecd2af734e63959da9009a6176ddf761f
Rue Ire <reerine20@msn.com>
2025-03-22 20:51:48 +0200

    Update insert.sql
    commentary added, changed writer -> author

--- database/insert.sql
+++ database/insert.sql
@@ -1,9 +1,18 @@
 /*ALL  are VARCHART
 Create backend possibility to add books/writers/comics
+
+ONLY for 'ADMIN' for now
+User tba later
+ONLY in ENGLISH for now
+
 Find out how to auto-add writer from books/comics to writer
+
+GENRE and RATING not (yet) mandatory. Might change later.
+Genre to be fantasy, historical, nonfiction... etc
+Rating planned to be e.g. YA = young adult, MA = Mature... this not set in stone.
 */
 
-INSERT INTO writer(author, country) VALUES
+INSERT INTO author(writer, country) VALUES
 ();
 INSERT INTO books(title, ISBN, genre, rating) VALUES
 ();
