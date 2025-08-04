49045cfeae57daccb8f39193b48646bc0b7f75f2
Rue Ire <reerine20@msn.com>
2025-08-04 16:48:05 +0300

    UPDATE createLibrary.sql
    better names, genre now its own table

--- database/createlibrary.sql
+++ database/createlibrary.sql
@@ -2,30 +2,44 @@
 CREATE DATABASE IF NOT EXISTS library_db;
 USE library_db;
 
-CREATE TABLE writer(
+CREATE TABLE author(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    author VARCHAR(50) NOT NULL,
-    country VARCHAR(50)
+    name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE books(
+CREATE TABLE book(
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     ISBN VARCHAR(20) UNIQUE,
-    genre VARCHAR(20),
-    rating VARCHAR(50),
-    FOREIGN KEY (author) REFERENCES writer(id)
+    language VARCHAR(50) not null,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES author(id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
+);
+-- all genres
+CREATE TABLE genre (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+--link books to genres
+CREATE TABLE book_genre (
+    book_id INT,
+    genre_id INT,
+    PRIMARY KEY (book_id, genre_id),
+    FOREIGN KEY (book_id) REFERENCES book(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genre(id)
+        ON DELETE CASCADE
 );
 
-CREATE TABLE comics(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    ISBN VARCHAR(20) UNIQUE,
-    genre VARCHAR(20),
-    rating VARCHAR(50),
-    FOREIGN KEY (author) REFERENCES writer(id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-);
+-- CREATE TABLE comic(
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(50) NOT NULL,
+--     ISBN VARCHAR(20) UNIQUE,
+--     genre VARCHAR(20),
+--     rating VARCHAR(50),
+--     FOREIGN KEY (author) REFERENCES writer(id)
+--         ON DELETE RESTRICT
+--         ON UPDATE CASCADE
+-- );
