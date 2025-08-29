ec6a61af1ec3818bee0465d77581c34a9adfc680
Rue Ire <reerine20@msn.com>
2025-08-29 19:03:24 +0300

    Update Quaries
    new query added

--- database/quaries.sql
+++ database/quaries.sql
@@ -11,3 +11,19 @@ SELECT isbn FROM book where title = 'Salamavaras';
 SELECT a.name FROM author a INNER JOIN book b ON a.id = b.author_id WHERE b.isbn = 9789511228387;
 
 SELECT title FROM book WHERE isbn = 9789511228387;
+
+-- names instead of id numbers
+SELECT b.id, b.title, a.name AS author, b.isbn, b.language, s.name AS series, b.release_year
+FROM book b
+INNER JOIN author a ON a.id = b.author_id
+INNER JOIN series s ON s.id = b.series_id;
+
+
+/*
+for later
+
+ALTER TABLE book 
+ADD COLUMN series_id INT NULL 
+AFTER title;
+
+*/
