64717e14f55fd957172e3016dc7c49c4b1236291
Rue Ire <reerine20@msn.com>
2025-10-23 21:47:34 +0300

    create connection.py
    connection to db. It's own thing to make sure earlier code didn't break.
    Connect later?

---
+++ Analytics/connection.py
@@ -0,0 +1,44 @@
+
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+
+
+#import pymysql
+
+# for .env:
+import os 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+DB_HOST= os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+def get_books_df():
+    try:
+
+        engine = create_engine(
+            f"mysql+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}",
+            echo=False #True shows sql queries
+            )
+        
+
+        query = """
+        SELECT language, release_year, series_id
+        FROM book
+        WHERE 1=1;
+        """
+
+        df = pd.read_sql(query, engine)
+        return df
+
+    except SQLAlchemyError as e:
+        print(f"SQLAlchemy error connecting to MariaDB database: {e}")
+    except Exception as e:
+        print(f'Error connecting to database: {e}')
+
+get_books_df()
