05177811e0b5a818c81022a2737f39c75518c19a
Rue Ire <reerine20@msn.com>
2025-11-30 18:30:14 +0200

    Update books.jsx
    no need for url for now

--- frontend/src/components/books.jsx
+++ frontend/src/components/books.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 
 export default function FetchBooks() {
-    const url = "http://localhost:8000";
+    // const url = "http://localhost:8000";
 
     const [fetchedAllWords, setFetchedAllWords] = useState([]);
 
     useEffect(() => {
         const AllBooks = async () => {
             try {
-                const response = await fetch(url + "/books");
+                const response = await fetch("/books");
                 if (response.ok) {
                     const data = await response.json();
                     //just data would be books object!
