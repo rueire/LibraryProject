d6a2569ba58251b04788de2defc1a1e3cca7a385
Rue Ire <reerine20@msn.com>
2025-12-05 14:15:10 +0200

    create Settings and UserPage
    testing for router, basics of how navigation will work here

---
+++ frontend/src/components/settingsPage.jsx
@@ -0,0 +1,17 @@
+
+export default function SettingsPage() {
+
+    return (
+        <div className="settinspage-wrap">
+            <div className="theme-info">
+                <p>here you may choose different theme for app</p>
+            </div>
+            <div className="theme-choices">
+                <div className="themes">
+                    <p>choice1</p>
+                    <p>choice2</p>
+                </div>
+            </div>
+        </div>
+    )
+}
