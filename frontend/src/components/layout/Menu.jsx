feef12b540bb7d3bdf77717ab3d59fa1068307a5
Rue Ire <reerine20@msn.com>
2026-04-18 17:54:32 +0300

    update Menu.tsx
    only mobile view now
    CSS improvement

--- frontend/src/components/layout/Menu.jsx
+++ frontend/src/components/layout/Menu.jsx
@@ -1,22 +1,36 @@
-import { NavLink } from "react-router"
+import { NavLink } from "react-router";
 
 export default function Menu() {
-
-    return(
-        <div className='menu'>
-            <div className="menu-buttons">
-                <div id="home-logo">
-                    <NavLink to='/'>Logo here</NavLink></div>
-                <div className="small-btns">
-                    <div id="user-btn">
-                        <NavLink to='/user'>User</NavLink>
-                    </div>
-                    <div id="faves-btn">faves</div>
-                    <div id="settings-btn">
-                        <NavLink to='/settings'>Settings</NavLink>
-                    </div>
-                </div>
-            </div>
+  return (
+    // <div className='menu'>
+    //     <div className="menu-buttons">
+    //         <div id="home-logo">
+    //             <NavLink to='/'>Logo here</NavLink></div>
+    //         <div className="small-btns">
+    //             <div id="user-btn">
+    //                 <NavLink to='/user'>User</NavLink>
+    //             </div>
+    //             <div id="faves-btn">faves</div>
+    //             <div id="settings-btn">
+    //                 <NavLink to='/settings'>Settings</NavLink>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+    <div className="menu">
+      <div className="menuButtons">
+        <div id="homeLogo">
+          <NavLink to="/">Logo here</NavLink>
         </div>
-    )
+
+        <div id="user-btn">
+          <NavLink to="/user">User</NavLink>
+        </div>
+        <div id="faves-btn">faves</div>
+        <div id="settings-btn">
+          <NavLink to="/settings">Settings</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
